1df5ce1074fe464ccf781cde7b91d18407ff440e
ShabAza83 <139683752+ShabAza83@users.noreply.github.com>
2024-08-26 15:26:59 +0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,23 +1,31 @@
+import sys
+
 def main(input_str: str) -> str:
     
     operators = {'+', '-', '*', '/'}
     
     try:
+        
         parts = input_str.split()
         
+       
         if len(parts) != 3:
-            raise ValueError("throws Exception")
+            raise ValueError("throws Exception")  
         
         a_str, operator, b_str = parts
         
+        
         if operator not in operators:
-            raise ValueError
+            raise ValueError("throws Exception")  
+        
         
         a = int(a_str)
         b = int(b_str)
         
+        
         if not (1 <= a <= 10) or not (1 <= b <= 10):
-            raise ValueError("throws Exception")
+            raise ValueError("throws Exception")  
+        
         
         if operator == '+':
             result = a + b
@@ -26,18 +34,17 @@ def main(input_str: str) -> str:
         elif operator == '*':
             result = a * b
         elif operator == '/':
-            result = a // b
-            
+            result = a // b  
+        
         return str(result)
     
     except Exception as e:
         return str(e)
+
+if __name__ == "__main__":
     
-print(main("1 + 2"))
-print(main("10 - 3"))
-print(main("6 * 9"))
-print(main("8 / 4"))
-print(main("10 / 3"))
-print(main("1"))
-print(main("15 + 25"))
-print(main("1 + 2 + 3 * 5"))
+    input_str = input("Введите выражение: ")
+    
+   
+    print(main(input_str))
+
